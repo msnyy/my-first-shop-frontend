20cd29c29030d62a5c6fbe931640990b2023a138
msny <mind29809@gmail.com>
2023-12-17 01:24:53 +0700

    Latest ud

--- my-first-shop-frontend/src/pages/AddGoods.js
+++ my-first-shop-frontend/src/pages/AddGoods.js
@@ -26,13 +26,13 @@ export default function AddGoods() {
     useEffect(() => {
         if (images.length < 1) return;
         const newImagesUrls = [];
-        images.forEach(image => newImagesUrls.push(URL.createObjectURL(image)))           
+        images.forEach(image => newImagesUrls.push(URL.createObjectURL(image)))
         setImageURLs(newImagesUrls);
-        }, [images]);
+    }, [images]);
 
-        function onImageChange(e) {
-            setImages([...e.target.files]);
-        }
+    function onImageChange(e) {
+        setImages([...e.target.files]);
+    }
 
 
 
@@ -74,16 +74,22 @@ export default function AddGoods() {
                             </div>
                         </form>
                     </div>
-                    <div class='flex justify-center h-64'>
-                        <div class="file-upload-box bg-sky-100 rounded-lg border-dashed border-4 border-violet-400 w-1/4">
-                            <div class='flex justify-center'>
-                                <input type="file" accept="image/*" onChange={onImageChange} />
-                                {imageURLs.map(imageSrc=> (
-                                <img class='w-2/3' src={imageSrc}/>)
-                                )}
+                    <div class="flex items-center my-5 space-x-10 w-full">
+                        <input id="dropzone-file" type="file" class="hidden" accept="image/*" onChange={onImageChange} />
+                        {imageURLs.map((image, index) => (
+                            <img class='w-1/6' key={index} alt={`Selected Image ${index + 1}`} src={image} />
+                        ))}
+                        <label for="dropzone-file" class="flex flex-col items-center justify-center w-1/5 h-56 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                            <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                                <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                                </svg>
+                                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
                             </div>
-                        </div>
+
+                        </label>
                     </div>
+
                 </div>
             </div>
         </div>
